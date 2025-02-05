37208bc52b6a597ceb218fd26c51036b970cd348
Iomannen <maxymdavidov@gmail.com>
2025-02-05 22:38:13 +0300

    added logic to tasks counter

--- todolist/src/components/input/input_components/button/Button.jsx
+++ todolist/src/components/input/input_components/button/Button.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import plus from "./plus.svg";
 import "./button.css";
+
+import Input from "../input/Input";
+
 class Button extends React.Component {
+  addTask = (event) => {
+    const input = document.querySelector("input");
+    localStorage.setItem(input.value, true);
+  };
   render() {
     return (
-      <button className="additionButton">
+      <button className="additionButton" onClick={this.addTask}>
         <div className="buttonLabel">Добавить</div>
         <img src={plus} className="buttonPlus"></img>
       </button>
--- todolist/src/components/tasks_counter/TasksCounter.jsx
+++ todolist/src/components/tasks_counter/TasksCounter.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import TasksCounterModule from "./tasks_counter_module/TasksCounterModule";
 import "./tasks_counter.css";
+
 class TasksCounter extends React.Component {
   render() {
     return (
       <div className="tasksCounter">
-        <TasksCounterModule content={"Все задачи"} counter={"0"} />
-        <TasksCounterModule content={"Завершено"} counter={"0 из 0"} />
+        <TasksCounterModule
+          content={"Все задачи"}
+          counter={`${localStorage.length}`}
+        />
+        <TasksCounterModule
+          content={"Завершено"}
+          counter={`${(() => {
+            const keys = Object.keys(localStorage);
+            let counter = 0;
+            for (let key of keys) {
+              if (localStorage.getItem(key) === "false") {
+                counter++;
+              }
+            }
+            return counter;
+          })()} из ${localStorage.length}`}
+        />
       </div>
     );
   }
